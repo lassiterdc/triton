8e89bcd6532cdabb13e6fa9fa9b9ac09a9c3ae99
Sudershan Gangrade <gangrades@ornl.gov>
2025-09-23 14:27:18 -0400

    Update allatoona.cfg and input, bug fix in par geotiff
    Former-commit-id: be3ae46d1ae235529e92b306c6226362a0d789c5

--- src/output.h
+++ src/output.h
@@ -643,12 +643,12 @@ namespace Output
 				if(strcmp(output_option_.c_str(), "SEQ") == 0)
 				{
 					write_output_geotiff_sequential(qx_arr, "QX", print_id, projection);
-					write_output_binary_sequential(h_arr, "QX", print_id);      // write binary as well for hotstart 
+					write_output_binary_sequential(qx_arr, "QX", print_id);      // write binary as well for hotstart 
 				}
 				else
 				{
 					write_output_geotiff_parallel(qx_arr, "QX", print_id, projection);
-					write_output_binary_parallel(h_arr, "QX", print_id);  // write binary as well for hotstart 
+					write_output_binary_parallel(qx_arr, "QX", print_id);  // write binary as well for hotstart 
 				}
 			}
 
@@ -657,12 +657,12 @@ namespace Output
 				if(strcmp(output_option_.c_str(), "SEQ") == 0)
 				{
 					write_output_geotiff_sequential(qy_arr, "QY", print_id, projection);
-					write_output_binary_sequential(h_arr, "QY", print_id);      // write binary as well for hotstart 
+					write_output_binary_sequential(qy_arr, "QY", print_id);      // write binary as well for hotstart 
 				}
 				else
 				{
 					write_output_geotiff_parallel(qy_arr, "QY", print_id, projection);
-					write_output_binary_parallel(h_arr, "QY", print_id);  // write binary as well for hotstart 
+					write_output_binary_parallel(qy_arr, "QY", print_id);  // write binary as well for hotstart 
 				}
 			}
 			
@@ -673,12 +673,12 @@ namespace Output
 					if(strcmp(output_option_.c_str(), "SEQ") == 0)
 					{
 						write_output_geotiff_sequential(max_value_h, "MH", print_id, projection);
-						write_output_binary_sequential(h_arr, "MH", print_id);      // write binary as well for hotstart 
+						write_output_binary_sequential(max_value_h, "MH", print_id);      // write binary as well for hotstart 
 					}
 					else
 					{
 						write_output_geotiff_parallel(max_value_h, "MH", print_id, projection);
-						write_output_binary_parallel(h_arr, "MH", print_id);  // write binary as well for hotstart 
+						write_output_binary_parallel(max_value_h, "MH", print_id);  // write binary as well for hotstart 
 					}
 				}
 			}
